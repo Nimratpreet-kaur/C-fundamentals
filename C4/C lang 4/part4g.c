4ae62e58d64dafc0c37f2dddb29f4ecbce9828ce
Nimratpreet kaur <nimratleo5@gmail.com>
2026-09-14 21:55:51 +0530

    Organize Part 3 files into subfolder

---
+++ C4/C lang 4/part4g.c
@@ -0,0 +1,13 @@
+// multidimensional array
+#include<stdio.h>
+int main()
+{
+    int marks[2][3]; //= {{98,95,99},{97,94,92}};//marks of 2 students in 3 subjects
+    int marks[0][0] = 98;
+    int marks[0][1] = 95;
+    int marks[0][2] = 99;
+    int marks[1][0] = 97;
+    int marks[1][1] = 94;
+    int marks[1][2] = 92;
+    return 0;
+}
---
+++ CProjects/projects/README.md
@@ -0,0 +1,2 @@
+# Number-guess-Game
+This repository contain code for number guess game in C language. This interesting game challenges users to guess random number secretly chosen by computer with hints of high and low and attempt tracks. 
