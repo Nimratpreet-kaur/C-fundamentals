76d761c4a4cfab735a83f092c5981507ddbf2c27
Nimratpreet kaur <nimratleo5@gmail.com>
2026-09-16 17:57:08 +0530

    Remove Duplicate element of sorted array

---
+++ Data structure.c/d.c
@@ -0,0 +1,30 @@
+// how to remove duplicate element in sorted array
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("Number of elements in sorted array: ");
+    scanf("%d",&n);
+    int arr[n],copy[n];
+    for(i=0;i<n;i++)
+    {
+        printf("Enter element %d of array: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    copy[0]=arr[0];
+    
+    int m=0;
+    for(i =1;i<n;i++)
+    {
+        if(arr[i]>copy[m])
+        {
+            copy[m+1]=arr[i];
+            m++;
+        }
+    }
+    for(i=0;i<=m;i++)
+    {
+        printf("%d ",copy[i]);
+    }
+    return 0;
+}
